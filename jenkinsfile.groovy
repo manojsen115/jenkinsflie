be19bc30fac4e89d9df5a3ecfff02ed59ceeb409
manojsen115 <36729893+manojsen115@users.noreply.github.com>
2018-12-20 13:39:55 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,33 +1,31 @@
 pipeline {
- 	agent any
- 	stages {
- 	stage('BUILD') {
- 	steps {
- 	echo 'Build start...'
- 	sleep 11
- 	echo 'Build finish...'
- 	}
- 	}
- 	stage('TEST') {
- 	steps {
- 	echo 'Test start...'
- 	sleep 11
- 	echo 'Test finish...'
- 	}
- 	}
- 	stage('DEPLOYEE') {
- 	steps {
- 	echo 'Deployee start...'
- 	sleep 11
- 	echo 'Deployee finish...'
- 	}
- 	}
- 	}
- 	post { 
- 	always { 
- 	echo 'Status:Success'
- 	}
- 	}
- 	}
- 
- 
+    agent any
+    stages {
+        stage('BUILD') {
+            steps {
+                echo 'Build start...'
+                sleep 11
+                echo 'Build finish...'
+            }
+        }
+        stage('TEST') {
+            steps {
+                echo 'Test start...'
+                sleep 11
+                echo 'Test finish...'
+            }
+        }
+        stage('DEPLOYEE') {
+            steps {
+                echo 'Deployee start...'
+                sleep 11
+                echo 'Deployee finish...'
+            }
+        }
+    }
+    post { 
+        always { 
+            echo 'Status:Success'
+        }
+    }
+}
