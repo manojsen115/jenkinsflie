185f403797d8abb36e42c3f1bbc250705ab4b6e0
manojsen115 <36729893+manojsen115@users.noreply.github.com>
2018-12-24 11:24:47 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,22 +1,43 @@
 pipeline {
     agent any
     stages {
+        
+        stage ('Build')
+		{
+		  // Run the gradle build for release
+			//sh "git config --global user.email \"jenkins@bestbuy.com\""
+			//sh "git config --global user.name \"jenkins\""
+			//sh "git tag -a $version -m \"[ release-build : $version ]\""
+			//sh "git push origin $version"
+			//rtGradle.run buildFile: 'build.gradle', tasks: 'clean build sonarqube dependencyCheck', switches: '--stacktrace'
+			//junit 'build/test-results/test/*.xml'
+			//step([$class: 'DependencyCheckPublisher'])  
+			//sh returnStdout: true, script: "sed -r 's/(version=)([0-9]+)(.)([0-9]+)(.)([0-9]+)/echo \\1\\2\\3\\4\\5\$\\((\\6+1))\\-SNAPSHOT/ge' -i gradle.properties"
+			//sh "git add gradle.properties && git commit -m \"[ release-build-complete ] : incrementing development snapshot version\" && git push origin master"
+		} 
+        	
+    stage ('Deploy Image to CI Test')
+		{
+		  // Deploying new container image
+		 // sh "/opt/apps/scripts/deployment/deployservice.py ${OPENSHIFT_TENANT_PROJECT_NAME}-test $service_name $version $region 1 ci 2181 port-forward"
+		} 
+ 
+
+        
+        
         stage('BUILD') {
             steps {
                 echo 'Build start...'
                 sleep 11
                 cleanWs()
-              
                 input message: 'Do you want to procreed?', ok: 'YES'
-                gradle {     
-        buildFile('build.gradle')
-          switches('--stacktrace')
-        tasks("clean build")
-          gradleName('Gradle')
-          useWrapper(false)
-      } 
- 
-
+                       gradle {     
+                              buildFile('build.gradle')
+                              switches('--stacktrace')
+                              tasks("clean build")
+                              gradleName('Gradle')
+                              useWrapper(false)
+                              }
                 echo 'Build finish...'
             }
         }
@@ -51,6 +72,19 @@ pipeline {
         }
     }
     post { 
+         
+        if ("$version".endsWith('-SNAPSHOT')) {
+         stage ('Finished')
+         echo "Finished the build process"
+        } 
+        else {
+            stage ('Finished') {
+             // Run veracode static scan
+             echo "Finished the build process but running a Veracode scan since this is a release build"
+            //veracode applicationName: "${VERACODE_APPLICATION_NAME}", createProfile: true, createSandbox: true, criticality: 'Medium', autoscan: true, fileNamePattern: '', pHost: 'usproxy.na.bestbuy.com', pPassword: '', pPort: 8080, pUser: '', replacementPattern: '', sandboxName: "${JOB_NAME}", scanExcludesPattern: '', scanIncludesPattern: '', scanName: "${JOB_NAME}-$version", uploadExcludesPattern: '', uploadIncludesPattern: '**/**.war', useIDkey: true, vid: "${VERACODE_ID}", vkey: "${VERACODE_KEY}", vpassword: '', vuser: ''
+         } 
+ 
+
         always { 
             echo 'Status:Success'
         }
